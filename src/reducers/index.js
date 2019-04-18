441b70ddcc80fd7e18767c5c9fae1d2f971d4df4
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-04-18 21:58:17 +0000

    Automatically backed up by Learn

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,15 @@
-import { combineReducers } from 'redux';
-import quotes from './quotes';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import rootReducer from './reducers/index'
 
-export default combineReducers({
-  quotes
-});
+let store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
